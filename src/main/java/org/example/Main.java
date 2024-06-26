d2e12938f93aafd582f7c39bf28b850c35a875d4
龙打野 <18128966990@163.com>
2024-06-26 18:09:48 +0800

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import static nu.pattern.OpenCV.loadShared;
+
 public class Main {
     public static void main(String[] args) {
+        loadShared();
         System.out.println("Hello world!");
     }
 }
