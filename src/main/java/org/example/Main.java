7b15f7d13d1668670f78b8b843a7accf67da6d30
龙打野 <18128966990@163.com>
2024-06-27 08:56:02 +0800

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,15 @@
 package org.example;
 
-import static nu.pattern.OpenCV.loadShared;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
 public class Main {
     public static void main(String[] args) {
-        loadShared();
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         System.out.println("Hello world!");
+        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+        System.out.println("mat: " + mat.dump());
     }
 }
